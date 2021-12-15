328f33efaff970ef745f7832be36d1323eba82c7
Zoe Zhang <zhangzhijuan23@gmail.com>
2021-12-15 19:56:44 +0100

    code check

--- src/Component/utilis/index.js
+++ src/Component/utilis/index.js
@@ -282,8 +282,4 @@ export const randomColor = () => {
     return colors[randomNum(1, 15)];
 };
 
-export const Icon = {
-    github: "http://www.nevergiveupt.top/github.png",
-    sf: "http://www.nevergiveupt.top/sf.jpeg",
-    zhihu: "http://www.nevergiveupt.top/zhihu.jpg",
-};
+
